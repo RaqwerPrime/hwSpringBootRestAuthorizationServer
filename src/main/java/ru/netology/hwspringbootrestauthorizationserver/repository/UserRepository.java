fc234bb2dad6f7731aaa205b5b62bd3adb70f7b1
Ruslan Araslanov <allmagrave@gmail.com>
2025-09-25 10:37:49 +0300

    add repository

---
+++ src/main/java/ru/netology/hwspringbootrestauthorizationserver/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package ru.netology.hwspringbootrestauthorizationserver.repository;
+
+public class UserRepository {
+    public List<Authorities> getUserAuthorities(String user, String password) {
+        return ...;
+    }
+
+}
